191c4ec000239805dc07135b63b3aec7db554c8c
SavrunE <igor1996igor@inbox.ru>
2020-10-02 14:41:36 +0300

    пофиксил прыжек
    прыгал больше на 1, чем должен

--- HyperCasual/Assets/1EndGameFolder/Scripts/Character/Character.cs
+++ HyperCasual/Assets/1EndGameFolder/Scripts/Character/Character.cs
@@ -12,34 +12,26 @@ public class Character : MonoBehaviour
     public float JumpPower { get { return jumpPower; } }
     [SerializeField] private float jumpPower = 3.5f;
 
-    private bool lookOnLeft  = false;
+    private bool lookOnLeft = false;
     private bool isGrounded = true;
     private bool canJump = true;
     private float jumpDelay = 1f;
-    private float checkJumpDelay = 1f;
-    [SerializeField] private int extraJumpValue = 2;
-    private int extraJump;
+    private int extraJump = 2;
+    private int extraJumpCheck;
     private bool jumpDirection;
 
-    Animator Animator;
+    Animator animator;
     Rigidbody2D body;
 
     [SerializeField] Transform groundCheker;
 
     void Start()
     {
-        Animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
     }
     void FixedUpdate()
     {
-        extraJump = extraJumpValue;
-        /// заменить на куратину
-        if (checkJumpDelay > 0)
-            checkJumpDelay -= Time.fixedDeltaTime;
-        else
-            canJump = true;
-        ///
         if (isGrounded)
             Move();
 
@@ -52,31 +44,41 @@ public class Character : MonoBehaviour
             Flip();
 
         CheckGround();
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && canJump)
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
-            checkJumpDelay = jumpDelay;
-            canJump = false;
+            StartCoroutine(JumpWaiter());
             Jump();
-
         }
     }
+    IEnumerator JumpWaiter()
+    {
+        canJump = false;
+        yield return new WaitForSeconds(jumpDelay);
+        extraJumpCheck--;
+        if (extraJumpCheck > 0)
+            canJump = true;
+
+    }
+
 
     private void CheckGround()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheker.position, 0.3f);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheker.position, 0.1f);
 
         isGrounded = colliders.Length > 1;
         /// сделать через касание ножками, каждый фрейм вызывать - это бред
+        /// UPD странно, все так делают...
         if (isGrounded)
         {
-            extraJump = extraJumpValue;
-            Animator.SetBool("IsGrounded", true);
+            canJump = true;
+            extraJumpCheck = extraJump;
+            animator.SetBool("IsGrounded", true);
         }
         else
         {
             SeparatorAnimation(body.velocity.y, "JumpUp");
-            Animator.SetBool("IsGrounded", false);
-            Animator.SetBool("Moving", false);
+            animator.SetBool("IsGrounded", false);
+            animator.SetBool("Moving", false);
         }
     }
 
@@ -84,11 +86,11 @@ public class Character : MonoBehaviour
     {
         if (velocityDirection > 0)
         {
-            Animator.SetBool(boolValue, true);
+            animator.SetBool(boolValue, true);
         }
         else
         {
-            Animator.SetBool(boolValue, false);
+            animator.SetBool(boolValue, false);
         }
 
     }
@@ -97,18 +99,18 @@ public class Character : MonoBehaviour
         body.velocity = new Vector2(Joystick.Horizontal * MoveSpeed, body.velocity.y);
         if (body.velocity.x == 0)
         {
-            Animator.SetBool("Moving", false);
+            animator.SetBool("Moving", false);
         }
         else
-            Animator.SetBool("Moving", true);
+            animator.SetBool("Moving", true);
     }
     private void Jump()
     {
         Vector2 moveInput = new Vector2(Joystick.Horizontal, Joystick.Vertical);
         body.AddForce(moveInput.normalized * JumpPower, ForceMode2D.Impulse);
-        canJump = false;
-        isGrounded = false;
         
+        isGrounded = false;
+
     }
     private void Flip()
     {
